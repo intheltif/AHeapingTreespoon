ed264c95ea1c85aca89c85c0edc832ab0c928f4d
Evert <weball1@catamount.wcu.edu>
2019-11-16 22:03:10 -0500

    Initial commit.

---
+++ Heap.py
@@ -0,0 +1,71 @@
+class Heap:
+    """ A heap. """
+
+    # TODO Most of the stuff below is just stubs for methods and accompanying 
+    #      documentation. This may need to be changed as well any instance
+    #      or class variables may need to be adjusted.
+
+    # Temporary storage for the paths starting at tempPath[1]
+    def __init__(self):
+        self.tempPath = []
+
+    def getTempPath(self):
+        """
+        Returns the temporary path
+        """
+        return self.tempPath
+
+    def readPaths(inputFile):
+        """ 
+        Reads inputFile given at the command line and places the contents of 
+        each line into the path field found in each PathNode object. The order 
+        is the same as found in the text file. Adds the PathNode object to the 
+        tempPath starting at tempPath[1].
+        
+        TODO Add paramater and return documentation
+
+        """
+        # TODO Finish readPaths function
+    
+    def buildCompleteTree(index, parent):
+        """
+        Recursively builds a complete binary tree. Places PathNode objects in 
+        tempPath into a complete binary tree in order of appearance in the text 
+        file. The left child of a parent located at tempPath[index] is found at 
+        tempPath[2 * index] and the right child is found at 
+        tempPath[(2 * index) + 1].
+
+        TODO Add paramater and return documentation
+
+        """
+        # TODO Finish buildCompleteTree function
+        
+    def setLevelEnd(root):
+        """ 
+        Recursive method that sets isLevelEnd.
+
+        TODO Add parameter documentation
+        """
+
+        # TODO Finish setLevelEnd function
+
+    def setGenerationLinks(root):
+        """
+        Recursive method that sets the "generation" link of PathNode object
+        from left-to-right. Generatoin is a term used to indicate a node on
+        the same level (these may be siblings or cousions).
+
+        TODO Add parameter documentation
+        """
+
+        # TODO Finish setGenerationLinks function
+
+    def printTreeLevels(root)
+        """
+        Prints the path lengths from left-to-right at each level in the tree in 
+        the form specified by the instructions.
+
+        TODO Add parameter documentation.
+        """
+
+        # TODO Finish printTreeLevels function
---
+++ PathNode.py
@@ -0,0 +1,85 @@
+class PathNode:
+    """ A node in a tree data structure. """
+    def __init__(self):
+        # An array of vertex IDs ordered by appearance in the path
+        self.path = []
+
+        # Reference to the left child
+        self.left = None
+
+        # Reference to the right child
+        self.right = None
+        
+        # Reference to the parent
+        self.parent = None
+
+        # Reference to the node directly to the right on the same tree level
+        self.generation = None # left to right sibling or cousion
+
+        # True if the node is the last in the level
+        self.isLevelEnd = False
+
+        # True if the node is the right most node
+        self.isLastNode = False
+
+    def getPath(self):
+        """
+        Returns an array of vertex IDs ordered by appearence in the path
+        """
+        return self.path
+
+   def getLeftChild(self):
+        """
+        Return the left node in this tree.
+        """
+        return self.left
+
+   def getRightChild(self):
+        """
+        Return the right node in this tree.
+        """
+        return self.right
+
+    def getParent(self):
+        """
+        Return the parent of this node.
+        """
+        return self.parent
+
+    def getGeneration(self):
+        """
+        Returns the node directly to the right of this node on the same 
+        tree level.
+        """
+        return self.generation
+
+    def setPath(self, path=[]):
+        """
+        Returns an array of vertex IDs ordered by appearence in the path
+        """
+        self.path = path
+
+   def setLeftChild(self, node):
+        """
+        Return the left node in this tree.
+        """
+        self.left = node
+
+   def setRightChild(self, node):
+        """
+        Return the right node in this tree.
+        """
+        self.right = node
+
+    def setParent(self, node):
+        """
+        Return the parent of this node.
+        """
+        self.parent = node
+
+    def setGeneration(self, node):
+        """
+        Returns the node directly to the right of this node on the same 
+        tree level.
+        """
+        self.generation = node
